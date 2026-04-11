bda690d0244e9d3bed4f39e0786b1fa78ccf2a36
Htgar <development@htgar.org>
2026-04-11 17:42:16 +0800

    Revert tailscale to layered install

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,9 +19,9 @@ dnf5 install -y adw-gtk3-theme podman-compose
 dnf5 remove -y tmux htop nvtop dconf-editor gnome-software gnome-software-rpm-ostree gnome-terminal-nautilus
 
 # Tailscale
-# dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-# dnf5 install -y tailscale
-# systemctl enable tailscaled
+dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf5 install -y tailscale
+systemctl enable tailscaled
 
 # Brave
 dnf5 remove -y firefox firefox-langpacks
